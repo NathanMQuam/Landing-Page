c5c6bff39077e85d7ab3f1070f941b5fe738e9fe
NathanMQuam <nathan.m.quam@gmail.com>
2021-08-05 20:55:23 -0600

    Update base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/landing-page/',
+  base: '/Landing-Page/',
   plugins: [vue()],
   build: {
     outDir: 'build',
