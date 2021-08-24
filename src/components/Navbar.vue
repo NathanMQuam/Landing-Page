7cf6804f76f09950ef07deb39660dcc471f175cf
Nathan Quam <Nathan.M.Quam@gmail.com>
2021-08-24 16:52:35 -0600

    Project components and projects page

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -32,6 +32,11 @@
             About
           </router-link>
         </li>
+        <li class="nav-item">
+          <router-link :to="{ name: 'Projects' }" class="nav-link">
+            Projects
+          </router-link>
+        </li>
       </ul>
       <!-- <span class="navbar-text">
         <button
