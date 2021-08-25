800ce7514a55911ce5962ebccdf6d8f0a0006f99
Nathan Quam <Nathan.M.Quam@gmail.com>
2021-08-25 17:16:07 -0600

    automatic build commit

--- assets/AboutPage.69f35062.js
+++
@@ -1,1 +0,0 @@
-import{o as a,a as e,b as o}from"./vendor.79fa1156.js";const t={name:"AboutPage"},s={class:"about"},n=o("h1",null,"This is the about page",-1);t.render=function(o,t,r,u,i,b){return a(),e("div",s,[n])};export default t;
