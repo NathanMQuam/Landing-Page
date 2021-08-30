23c6a023b4fec8ea3ecec4a47e81de36431d15d7
Nathan Quam <Nathan.M.Quam@gmail.com>
2021-08-30 15:20:04 -0600

    Added Bug Log to projects

--- src/AppState.js
+++ src/AppState.js
@@ -18,27 +18,20 @@ export const AppState = reactive({
     description: `This is Prompetition, a competitive creative writing application. There is one writing prompt that everyone can see, and everyone can submit their written work once. During that day, a user can only see their own submission. The next day everyone can vote on yesterday's entries for the one they like the most. At the end of that day, a winner is permanently decided, and this is displayed on their account profile.\n
     At any point afterwards, users can still enter submissions to previous writing prompts, but these post-contest submissions are only for artistic writing, there will not be a new winner. But, users can still "like" these post-contest submissions, and the highest scoring submission is put on display underneath the contest-period winner.\n
     There is also an alternative head-to-head mode where two users are put together and are given the same writing prompt, and a short period of time to write. Once this time is up, the two submissions are voted on by other users.`,
-    skillsUsed: ['HTML', 'CSS', 'JavaScript', 'Vue 3', 'Mongo DB', 'Node JS'],
+    skillsUsed: ['HTML', 'CSS', 'JavaScript', 'Vue 3', 'Mongo DB', 'Node JS', 'OAuth'],
     projectLink: 'https://prompetition-1.herokuapp.com/',
     sourceLink: 'https://github.com/NathanMQuam/Prompetition'
   }), new Project({
-    projectName: 'Project 2',
+    id: 'bug-log',
+    projectName: 'Bug Log',
     isFeatured: true,
     type: 'Web Application',
-    heroImage: 'https://picsum.photos/id/1002/800/600',
-    images: ['https://picsum.photos/id/1003/400/300', 'https://picsum.photos/id/1004/400/300'],
-    shortDescription: 'The second placeholder project',
-    description: 'This is the long description of the second placeholder project.',
-    skillsUsed: ['HTML', 'CSS', 'JavaScript', 'Vue 3']
-  }), new Project({
-    projectName: 'Project 3',
-    isFeatured: false,
-    type: 'Web Server',
-    heroImage: 'https://picsum.photos/id/1033/800/600',
-    images: ['https://picsum.photos/id/1040/400/300', 'https://picsum.photos/id/1042/400/300'],
-    shortDescription: 'Just a placeholder for a web server project',
-    description: 'The long description of the web server project',
-    skillsUsed: ['C#', 'SQL']
+    heroImage: '../src/assets/img/Bug-Log-Hero.jfif',
+    images: ['../src/assets/img/Bug-Log.jpg', '../src/assets/img/Open-Bug.jpg', '../src/assets/img/Open-Bug.jpg'],
+    shortDescription: 'A standard web application for tracking bugs.',
+    description: 'Open a bug and give it a title and description to start tracking. Comment on a bug to note suggestions or it\'s current status. Close bugs to mark them as resolved.',
+    skillsUsed: ['HTML', 'CSS', 'JavaScript', 'Vue 3', 'Mongo DB', 'Node JS', 'OAuth'],
+    sourceLink: 'https://github.com/NathanMQuam/Bug-Log'
   })],
   skills: [
     new Skill({ name: 'HTML 5', image: 'https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg' }),
